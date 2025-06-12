cff9e9de1fce702a244a5a8e9ee6106f462736a2
thughari <thughari3@gmail.com>
2025-06-12 08:38:07 +0530

    added keep alive

---
+++ src/main/java/com/thughari/randomchat/config/KeepAlive.java
@@ -0,0 +1,21 @@
+package com.thughari.randomchat.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class KeepAlive {
+
+	private static final Logger logger = LoggerFactory.getLogger(KeepAlive.class);
+	
+	@Scheduled(fixedRate = 840_000)
+	@Async("virtualThreadTaskExecutor")
+	public void keepAliveLog() {
+		logger.info("KeepAlive ping at {}", System.currentTimeMillis());
+	}
+}
