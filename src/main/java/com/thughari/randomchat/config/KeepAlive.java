89451d0bd9b4359ea8db2c43ed788b215250697a
Hari <thughari3@gmail.com>
2025-09-28 21:25:35 +0530

    Update KeepAlive.java

--- src/main/java/com/thughari/randomchat/config/KeepAlive.java
+++ src/main/java/com/thughari/randomchat/config/KeepAlive.java
@@ -1,33 +1,33 @@
-// package com.thughari.randomchat.config;
+package com.thughari.randomchat.config;
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.scheduling.annotation.Async;
-// import org.springframework.scheduling.annotation.EnableScheduling;
-// import org.springframework.scheduling.annotation.Scheduled;
-// import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
-// @Configuration
-// @EnableScheduling
-// public class KeepAlive {
+@Configuration
+@EnableScheduling
+public class KeepAlive {
 
-// 	private static final Logger logger = LoggerFactory.getLogger(KeepAlive.class);
+	private static final Logger logger = LoggerFactory.getLogger(KeepAlive.class);
 
-// 	@Value("${app.keepalive.url}")
-// 	private String pingUrl;
+	@Value("${app.keepalive.url}")
+	private String pingUrl;
 
-// 	private final RestTemplate restTemplate = new RestTemplate();
+	private final RestTemplate restTemplate = new RestTemplate();
 
-// 	@Scheduled(fixedRate = 300_000)
-// 	@Async("virtualThreadTaskExecutor")
-// 	public void keepAliveLog() {
-// 		try {
-// 			String response = restTemplate.getForObject(pingUrl, String.class);
-// 			logger.info("KeepAlive self-ping successful: {}", response);
-// 		} catch (Exception e) {
-// 			logger.error("KeepAlive self-ping failed", e);
-// 		}
-// 	}
-// }
+	@Scheduled(fixedRate = 300_000)
+	@Async("virtualThreadTaskExecutor")
+	public void keepAliveLog() {
+		try {
+			String response = restTemplate.getForObject(pingUrl, String.class);
+			logger.info("KeepAlive self-ping successful: {}", response);
+		} catch (Exception e) {
+			logger.error("KeepAlive self-ping failed", e);
+		}
+	}
+}
